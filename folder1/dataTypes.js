c95f4b4cb8c402bd5b9d622fc010e289baf3ecd9
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-07-30 11:44:51 +0000

    datatypes

---
+++ folder1/dataTypes.js
@@ -0,0 +1,21 @@
+let age = 21//numeric
+let isLoggedIn = false//boolean
+let state;//null
+let Name = "James Cameron"// string
+
+console.log(Name)
+
+//To know the type of a variable 'typeof' operator is used
+console.log(typeof age)
+
+/*
+JavaScript has 8 Datatypes
+String
+Number
+Bigint
+Boolean
+Undefined
+Null-> it is an object type
+Symbol
+Object
+*/
