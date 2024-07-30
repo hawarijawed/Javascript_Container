2d15d2f00727f86d085b5ae21a850c5b8b2afd3f
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-07-30 11:22:43 +0000

    committed

--- folder1/program01.js
+++
@@ -1,1 +0,0 @@
-console.log("Chin tapak dum dum");
---
+++ folder1/variables.js
@@ -0,0 +1,8 @@
+console.log("Chin tapak dum dum");
+const platform = 'Github'// can't be changed
+let editor = "vscode" // used most of the time
+var directory = "javaScript_container" //has scope issues
+num = 234//numeric value
+let val// undefined
+console.table([platform,editor,directory,num,val])
+
