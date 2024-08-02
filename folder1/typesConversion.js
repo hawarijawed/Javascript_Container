d563656c407289e2a7f1ab8f16e86672d72f438e
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-08-02 13:44:51 +0000

    JavaScript

--- folder1/typesConversion.js
+++ folder1/typesConversion.js
@@ -1,10 +1,10 @@
 
 //take a string variable
 let var1 = "345ad6"
-console.log(typeof var1);   // either typeof(var1) or typeof var1 both works
+//console.log(typeof var1);   // either typeof(var1) or typeof var1 both works
 let var1Int = Number(var1)
-console.log(typeof(var1Int));
-console.log("Converted string to number: ",var1Int);
+// console.log(typeof(var1Int));
+// console.log("Converted string to number: ",var1Int);
 
 //Note: For conversion to Number
 //1. Numeric string --> Numeric Value[e.g "33" --> 33]
@@ -17,6 +17,15 @@ console.log("Converted string to number: ",var1Int);
 //3. "abcd" --> true[Strings with value converts to true]
 
 // Conversion to String| Syntax: String(var)
+//____________________Special Conversion________________________//
+
+// console.log(1 + 3)//
+// console.log("1" + 3) //If first value is of string data type-> converts rest of the value in string
+// console.log("1" + 3 + 2)
+// console.log(1 + 3 + "2")//First add numeric value and then converts to string and add 2
+// console.log(1 + "3" + 5)//converts to string 
+// console.log(+true); //1
+// console.log(+""); //0
 
 
 
