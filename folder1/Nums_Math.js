4ca4a94c086cca1c0143917f74b8f6df4d3862e8
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-08-04 18:43:27 +0000

    Numbers and Math in JS

---
+++ folder1/Nums_Math.js
@@ -0,0 +1,31 @@
+// const score = 400;
+// const number = new Number(4000);
+// console.log(typeof(score));
+// console.log(typeof(number));
+// // console.log(score);
+// // console.log(number);
+// console.log(typeof score.toString());
+// let val = 7/3;
+// console.log(val.toFixed(3)); //shows value upto 3 decimal points
+// let balance = 100000000;
+// console.log(balance.toLocaleString('en-IN'));
+// console.log(balance.toExponential());
+
+//_____________________________MATH FUNCTIONS_____________________________//
+// console.log(Math.abs(-45));
+// console.log(Math.sqrt(49));
+// console.log(Math.round(4.8));//5
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+//Math.random() -> x => 0<=x<=1
+console.log(Math.random());
+//In order to set the minimum bar of value 
+let min = 10
+let max = 20
+console.log(Math.floor((Math.random()*(max-min+1))+min));//The value will always be in range 10 - 20
+
+
+
+
+
