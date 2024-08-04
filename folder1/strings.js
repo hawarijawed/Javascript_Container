6f485042b3ba5143dd6b63a0858c19503683b86e
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-08-04 18:14:08 +0000

    strings

---
+++ folder1/strings.js
@@ -0,0 +1,19 @@
+// const name = "Peter Parker"
+// const name1 = new String("Peter Parker");
+// const nickName = "Spider man"
+// // console.log(name+nickName);
+// console.log(`Hello you are ${name} people call you ${nickName}`);
+const gameName = "GODOFWAR";
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('W'));
+//gameName.trim() //trucates whitespaces from beginning and end
+// let newString = gameName.substring(0,4);
+// let SlicedString = gameName.slice(0,-2);
+// console.log(newString);
+// console.log(SlicedString);
+
+
+
+
