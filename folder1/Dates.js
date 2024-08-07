9e4d457b52e7eb563e1023b06b3101fe3c19fc0a
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-08-07 19:39:40 +0000

    Dates in JavaScipt

---
+++ folder1/Dates.js
@@ -0,0 +1,20 @@
+//Accessing date in javascript
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// Declaring date manually
+// let manualDate = new Date(2024,7,8);
+// console.log(manualDate.toString());
+// console.log(manualDate.toDateString());
+// console.log(manualDate.toLocaleString());
+
+// Declaring date in mm-dd-yyyy formate
+let date1 = new Date("05-23-2001");
+// console.log(date1.toString());
+// console.log(date1.toDateString());
+// console.log(date1.toLocaleString());
+console.log(date1.getHours());
+
