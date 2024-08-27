6862e8e84e4d024364a0a0287b6001a45b7fd034
Jawed Hawari <118849022+hawarijawed@users.noreply.github.com>
2024-08-27 20:19:00 +0000

    While N do While loop

---
+++ folder1/loops.js
@@ -0,0 +1,20 @@
+
+// let index = 0;
+// while (index<20) {
+//     console.log(index);
+//     index++;
+// }
+
+// let myArr = ['Super Man','Flash','Bat Man','Wonder Woman'];
+// let i=0;
+// while (i<myArr.length) {
+//     console.log(`Hero name: ${myArr[i]}`);
+//     i++;
+// }
+
+let score = 1;
+do {
+    console.log(`score: ${score}`);
+    score++;
+    
+} while (score<11);
